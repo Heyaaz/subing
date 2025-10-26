01f223d5af0c02b1a472794910d07beb923028e7
Peter <pion0458@gmail.com>
2025-10-26 10:16:34 +0900

    init

---
+++ src/main/java/com/project/subing/SubingApplication.java
@@ -0,0 +1,13 @@
+package com.project.subing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SubingApplication.class, args);
+  }
+
+}
